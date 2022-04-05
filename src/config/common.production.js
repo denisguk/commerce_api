41d83151a98578f139c087d34891e01a44d2bcd0
Denis Guk <denis.hooke@gmail.com>
2022-04-06 01:25:31 +0300

    Added prod config

--- src/config/common.production.js
+++ src/config/common.production.js
@@ -1,3 +1,12 @@
 export default {
     TOKEN_KEY: process.env.TOKEN_KEY,
+    BCRYPT_SALT: 10,
+    CLIENT_URL: "http://localhost:3000",
+    EMAIL_TRANSPORT: {
+        HOST: "smtp.gmail.com",
+        SERVICE: "gmail",
+        PORT: 587,
+        USER: process.env.EMAIL_TRANSPORT_USER,
+        PASS: process.env.EMAIL_TRANSPORT_PASS,
+    }
 }
