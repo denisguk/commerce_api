bdb7d8f41eb28b3168c95833d91e3dfde65e3620
Denis Guk <denis.hooke@gmail.com>
2022-04-08 10:14:38 +0300

    Fixed validator

--- src/entity/User/validators.js
+++ src/entity/User/validators.js
@@ -7,7 +7,13 @@ const getForgotPassword = [
 const getForgotPasswordRestore = [
     body('email').isString().isEmail(),
     body('token').isString(),
-    body('password').isString().equals('confirmPassword').withMessage("Password should match to confirmPassword field"),
+    body('password').isString().custom((value,{req}) => {
+        if (value !== req.body.confirmPassword) {
+            throw new Error("Password should match to confirmPassword field");
+        } else {
+            return value;
+        }
+    }),
     body('confirmPassword').isString().notEmpty().withMessage("Confirm Password should not be empty"),
 ];
 
